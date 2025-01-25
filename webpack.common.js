329cdfff102ce698bf047d4b0d7ef8015d384d9d
Tapio Jokinen <tapio.jokinen@hotmail.com>
2025-01-25 23:32:40 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: {
+    app: './js/app.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.svg'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './html/index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './assets', to: './assets' },
+        { from: './css', to: './css' },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  optimization: {
+    minimize: isProduction,
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
+};
