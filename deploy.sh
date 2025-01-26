8c494655636fedce4ce0b29ea3ac99d440e48415
Tapio Jokinen <tapio.jokinen@hotmail.com>
2025-01-26 11:20:17 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+cd ~/jokinen.dev
+
 docker buildx build . -t jokinen.dev
 
 docker run -d jokinen.dev
 
 CONTAINER_ID=$(docker container ls --all --filter=ancestor=jokinen.dev --format "{{.ID}}" | head -n 1)
+
+if [ -z "$CONTAINER_ID" ]; then
+    echo "Error: No container found for jokinen.dev."
+    exit 1
+fi
+
 
 docker cp "$CONTAINER_ID":/app/dist/. ./dist
 
@@ -14,7 +22,12 @@ docker stop "$CONTAINER_ID"
 
 docker rm "$CONTAINER_ID"
 
-WEBSERVER_CONTAINER_ID=$(docker ps -f name=YOUR_SERVICE_NAME --format "{{.ID}}" | head -n 1)
+WEBSERVER_CONTAINER_ID=$(docker ps -f name=reverse_proxy --format "{{.ID}}" | head -n 1)
+
+if [ -z "$WEBSERVER_CONTAINER_ID" ]; then
+    echo "Error: Web server container not found."
+    exit 1
+fi
 
 docker cp ./dist/. "$WEBSERVER_CONTAINER_ID":/var/www/html
 
